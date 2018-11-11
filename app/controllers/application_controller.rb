fb646eb6e67283618febbcc71181c9424dfa5e25
Joseph Arias <arias.joseph.a@gmail.com>
2018-11-11 17:57:41 -0500

    Login Required Lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    session[:name] ||= nil
+  end
+
+  def is_login
+    if current_user.nil? || current_user.empty?
+      redirect_to login_path
+    end
+  end
+
+  def welcome
+    if current_user.nil? || current_user.empty?
+      redirect_to login_path
+    else 
+      render :welcome
+    end
+  end
 end
---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,6 @@
+class SecretsController < ApplicationController
+  before_action :is_login
+  def show
+    
+  end
+end
